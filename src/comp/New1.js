21da0fe26ab207c9104a336e381ae4827b352d9d
aviobox <36160204+aviobox@users.noreply.github.com>
2018-03-07 17:52:43 +0700

    Add files via upload

---
+++ src/comp/New1.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class New1 extends Component{
+    render(){
+        return(
+            <h1>{this.props.nm}</h1>
+        )
+    }
+}
+export default New1;
